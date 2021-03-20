e332ea2e3c0b6f72c6d4a7321e453c3dd40a669a
Ivo Fernandes <ivomuze@gmail.com>
2021-03-20 11:14:47 +0000

    Step 6: Delete on click and pull refresh

--- flutter_challenge/lib/screen/listEmojiScreen.dart
+++ flutter_challenge/lib/screen/listEmojiScreen.dart
@@ -1,30 +1,53 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_challenge/state/AppStateProvider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class ListEmojiScreen extends StatelessWidget{
   AppStateProvider appState;
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
 
   ListEmojiScreen(this.appState);
 
   @override
   Widget build(BuildContext context) {
-    Widget listEmoji = renderListEmoji();
+    Widget listEmoji = renderListEmoji(context);
     return this.appState.getEmoji().isNotEmpty ?
             listEmoji : CircularProgressIndicator();
   }
 
-  Widget renderListEmoji() {
+  Widget renderListEmoji(BuildContext context) {
     List<String> emoji = this.appState.getEmoji();
-    return GridView.builder(
+    GridView grid = GridView.builder(
       itemCount: emoji.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 4,
       ),
       itemBuilder: (BuildContext context, int index){
-        return Image.network(emoji[index]);
+        return GestureDetector(
+            child: Image.network(emoji[index]),
+            onTap: () {
+              emoji.removeAt(index);
+              appState.refresh();
+            },
+        );
       },
     );
+
+
+    return SmartRefresher(
+      enablePullDown: true,
+      enablePullUp: false,
+      header: WaterDropHeader(),
+      controller: _refreshController,
+      onRefresh: () => _onRefresh(context),
+      child: grid,
+    );
+  }
+
+  void _onRefresh(BuildContext context) async{
+    await this.appState.loadPreferences(context);
+    _refreshController.refreshCompleted();
   }
 
 }
